039cce403db22ed4db615fe6c11b62ebc22b67f9
Ademir de Lucca Junior <ademir.junior@al.infnet.edu.br>
2025-06-23 21:52:33 -0300

    Update and rename Controle.java to Controller.java

--- ATDR3/src/main/java/Ex/Controller.java
+++ ATDR3/src/main/java/Ex/Controller.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class Controle {
+public class Controller {
     // Lista estática para armazenar usuários em memória
     private static final List<User> users = new ArrayList<>();
 
